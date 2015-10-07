14b3a6759a3529d7c58ed2046e5b4701d1633ac4
Jianming Chen <loafbaker@hotmail.com>
2015-10-07 15:25:54 +0800

    Add context in template files

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ django_ecommerce1
 A typical eCommerce site built by django framework
 
 Run in django 1.6 and sqlite3
+
+Ver.1	[Products App Views & Templates](../../tree/b66ce5397ecd8894d2d69c444d179a50defb8018)
--- products/views.py
+++ products/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    template = 'home.html'
-    context = locals()
+    if request.user.is_authenticated():
+        username_is = "Justin using context"
+        context = {"username_is": request.user}
+    else:
+        context = {"username_is": "unknown"}
+
+    template = "base.html"
     return render(request, template, context)
