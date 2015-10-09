8f2c6710eea8aefb94b4e65a9c0961148ebf3d11
Jianming Chen <loafbaker@hotmail.com>
2015-10-10 04:55:12 +0800

    Update product page to show product info

--- django_ecommerce1/urls.py
+++ django_ecommerce1/urls.py
@@ -13,3 +13,6 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 )
 
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
