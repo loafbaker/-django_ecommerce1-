3f7279ac24875b53873eac5febf02d8ca18717e6
Jianming Chen <loafbaker@hotmail.com>
2015-11-19 04:42:32 +0800

    Add logout view

--- django_ecommerce1/urls.py
+++ django_ecommerce1/urls.py
@@ -18,6 +18,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/logout/&', 'accounts.views.logout_view', name='auth_logout'),
 )
 
 if settings.DEBUG:
